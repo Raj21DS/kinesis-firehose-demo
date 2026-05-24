148fed325ef4c3dec061495848fb42959214dc18
Raj Kumar <Analyst.k.raj@gmail.com>
2026-05-24 21:09:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ youtube_urls = [
 
     "https://www.twitch.tv/valorant_pacific",
 
-    "https://www.youtube.com/watch?v=LPZh9BOjkQs",
+    "https://www.instagram.com/p/DTshte0CK2z/",
 
     "https://www.youtube.com/watch?v=X8F9JfCUWrs",
 
